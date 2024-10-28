865208509d87b037fdd08db3a5c17fbed2726ea3
Bulat <ninza147258@gmail.com>
2024-10-28 22:24:39 +0300

    add check interface

--- internal/service/topic/service.go
+++ internal/service/topic/service.go
@@ -1,6 +1,10 @@
 package topic
 
+import "github.com/Bulat147/rtclif-topic-service/internal/service"
+
 // здесь можно объявить интерфейс репозитория, а не в модуле репозитория
+
+var _ service.TopicService = (*TopicServiceImpl)(nil) // эта штука показывает, реализует ли наша структура нужный интерфейс
 
 type TopicServiceImpl struct {
 }
