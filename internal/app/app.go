665b46f893e9cdd16cbf598b7b0a9db776578fc1
Bulat <ninza147258@gmail.com>
2024-11-04 21:54:25 +0300

    refactor app

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"context"
 	"log"
 	"net"
 
@@ -15,21 +16,55 @@ type App struct {
 	grpcServer      *grpc.Server
 }
 
-func NewApp() (*App, error) {
-	app := &App{serviceProvider: newServiceProvider()}
-	app.grpcServer = grpc.NewServer()
-
-	reflection.Register(app.grpcServer)
-	topic.RegisterTopicV1Server(app.grpcServer, app.serviceProvider.TopicServer())
-
-	if err := config.Load(".env"); err != nil {
-		return nil, err
-	}
+func NewApp(ctx context.Context) (*App, error) {
+	app := &App{}
+	app.initDeps(ctx)
 
 	return app, nil
 }
 
 func (a *App) Run() error {
+	return a.runGrpcServer()
+}
+
+func (a *App) initDeps(ctx context.Context) error {
+	var deps [3]func(context.Context) error = [3]func(context.Context) error{
+		a.initConfig,
+		a.initGrpcServer,
+		a.initServiceProvider,
+	}
+
+	for _, f := range deps {
+		err := f(ctx)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (a *App) initConfig(_ context.Context) error {
+	if err := config.Load(".env"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *App) initGrpcServer(_ context.Context) error {
+	a.grpcServer = grpc.NewServer()
+
+	reflection.Register(a.grpcServer)
+	topic.RegisterTopicV1Server(a.grpcServer, a.serviceProvider.TopicServer())
+	return nil
+}
+
+func (a *App) initServiceProvider(_ context.Context) error {
+	a.serviceProvider = newServiceProvider()
+	return nil
+}
+
+func (a *App) runGrpcServer() error {
 	log.Printf("Grpc Server is running on %s", a.serviceProvider.GrpcConfig().Address())
 
 	listener, err := net.Listen("tcp", a.serviceProvider.GrpcConfig().Address())
